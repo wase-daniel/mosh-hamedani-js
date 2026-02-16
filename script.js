459c1e89ea38d1560a93947460951eb58e4b9299
wase daniel <wasedaniel01@gmail.com>
2026-02-16 10:24:33 -0800

    review on js 3

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-var mun = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [[10, 11, 12], 13, 14],
-];
-var min = mun[2][1];
-console.log(min);
+var ourArray = ["daniel", "wase", "sewa"];
+ourArray.push(["happy", "day"]);
+console.log(ourArray);
