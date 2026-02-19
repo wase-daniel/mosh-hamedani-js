5250d7e7021b6659267a35f76d167213993659e7
wase daniel <wasedaniel01@gmail.com>
2026-02-19 00:07:19 -0800

    view on interview of amalitech

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function myLocalScope() {
-  localScope = 12;
-  console.log(localScope);
+function solution(numbers) {
+  let result = [];
+
+  for (let i = 0; i < numbers.length - 2; i++) {
+    let a = numbers[i];
+    let b = numbers[i + 1];
+    let c = numbers[i + 2];
+
+    if ((a < b && b > c) || (a > b && b < c)) {
+      result.push(1);
+    } else {
+      result.push(0);
+    }
+  }
+
+  return result;
 }
-myLocalScope();
-console.log(localScope);
