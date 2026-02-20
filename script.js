972a444aba1ce7e27b828fe0b1f631ca2eae2827
wase daniel <wasedaniel01@gmail.com>
2026-02-20 06:22:02 -0800

    learning else if statement

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function checkNum(num) {
-  if (num > 21 || num < 11) {
-    return "outside";
+function elseIF(num) {
+  if (num > 10) {
+    return "number is greater than 10";
+  } else if (num < 5) {
+    return "number is less than 5";
+  } else {
+    return "between 10 and 5";
   }
-  return "inside";
 }
-console.log(checkNum(22));
+console.log(elseIF(8));
