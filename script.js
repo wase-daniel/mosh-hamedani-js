6298cfb257640e7c6892126e4d2e0a2b2e890e5b
wase daniel <wasedaniel01@gmail.com>
2026-02-20 02:19:59 -0800

    learning boolean

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-let width = 0;
-const progressBar = document.getElementById("myBar");
-
-function startLoading() {
-  setInterval(() => {
-    if (width < 99) {
-      // It stops at 99% and never hits 100!
-      width++;
-      progressBar.style.width = width + "%";
-    }
-  }, 100); // Moves every 0.1 seconds
+function ourFunctionStatement(isItTrue) {
+  if (isItTrue) {
+    return "yes it's true";
+  } else {
+    return "no it's false";
+  }
 }
+console.log(ourFunctionStatement(false));
