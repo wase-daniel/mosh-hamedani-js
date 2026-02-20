d9b2da0d676126b7ccf70981a9db5b1a11d67169
wase daniel <wasedaniel01@gmail.com>
2026-02-20 00:54:20 -0800

    funny code u can't click on link

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var changed;
-function changeNum(num) {
-  return (num + 5) / 3;
-}
-console.log(changeNum(5));
+const button = document.getElementById("prankBtn");
+
+button.addEventListener("mouseover", () => {
+  // This moves the button to a random spot on the screen
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 50);
+
+  button.style.left = `${x}px`;
+  button.style.top = `${y}px`;
+});
