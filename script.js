77f7966ce3b2a71235af45b71ab4ca8928f4a34b
wase daniel <wasedaniel01@gmail.com>
2026-02-23 08:47:30 -0800

    aria challenge lol 3

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-function calculateMealPrice(items, quantity) {
-  var quantity = "";
-  var price = "";
-  switch (items) {
-    case "inyama":
-      price = 2000 * quantity;
-      break;
-    case "inishyimbo":
-      price = 1000 * quantity;
-      break;
-    case "umuceri":
-      price = 1500 * quantity;
+function ariaChallenge(val) {
+  switch (val) {
+    case "umutuku":
+      return "red";
+    case "ubururu":
+      return "blue";
+    case "icyatsi":
+      return "green";
     default:
-      price = "ibyo ntabwo tubifite";
+      return "ntabwo nzi iryo bara";
   }
-  return price;
 }
-console.log(calculateMealPrice("inyama", 2));
