7a994338509052f5d61944e4abc8efbc9b62d7b6
wase daniel <wasedaniel01@gmail.com>
2026-02-23 07:49:01 -0800

    aria challenge lol

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function mulwitch(a, b) {
-  return (a, b < 0);
+function ariaChallenge(val) {
+  var color = "";
+  switch (val) {
+    case "umutuku":
+      color = "red";
+      break;
+    case "ubururu":
+      color = "blue";
+      break;
+    case "icyatsi":
+      color = "green";
+      break;
+    default:
+      color = "ntabwo nzi iryo bara";
+  }
+  return color;
 }
-console.log(mulwitch(-21, -2));
+console.log(ariaChallenge("umutuku"));
