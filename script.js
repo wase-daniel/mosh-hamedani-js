9292bf57492311bbc7fbfd8d79f571d590ab8e17
wase daniel <wasedaniel01@gmail.com>
2026-02-24 09:45:34 -0800

    accessing object using [] 3

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var ObjectList = {
   "my name": "daniel",
   "the age": 20,
   "your height": 62,
+  gander: "male",
 };
 
 var acceses = ObjectList["your height"];
+var accesesVal = ObjectList.gander;
 console.log(acceses);
