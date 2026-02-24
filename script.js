c354e86ebdd659132d1beb356ffe8990827f50ff
wase daniel <wasedaniel01@gmail.com>
2026-02-24 08:30:32 -0800

    object

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-function calculateMealPrice(item, quantity) {
-  let price;
-
-  switch (item) {
-    case "Inyama":
-      price = 2000;
-      break;
-    case "Ibishyimbo":
-      price = 1000;
-      break;
-    case "Umuceri":
-      price = 1500;
-      break;
-    default:
-      return "Ibyo ntabwo tubifite!";
-  }
-
-  return price * quantity + " RWF";
-}
+var myDog = {
+  name: "kazungu",
+  legs: 4,
+  tail: 1,
+};
+console.log(myDog);
