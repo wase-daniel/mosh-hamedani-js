07823192d00a2ae9e57221c3ae94654496d73929
wase daniel <wasedaniel01@gmail.com>
2026-02-25 01:47:34 -0800

    accessing object using [] 15

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function lookUp(val) {
   var general = "";
   var answer = {
-    first: "monday",
-    second: "tuesday",
-    third: "wednesday",
-    fourth: "thursday",
-    firth: "friday",
+    1: "monday",
+    2: "tuesday",
+    3: "wednesday",
+    4: "thursday",
+    5: "friday",
   };
   general = answer[val];
   return general;
 }
-console.log(lookUp(second));
+console.log(lookUp(1));
