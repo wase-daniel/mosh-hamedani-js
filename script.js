5d4a333455d08ae86f6898907147cefc0720f674
wase daniel <wasedaniel01@gmail.com>
2026-02-25 01:39:27 -0800

    accessing object using [] 14

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var testObject = {
-  "my name": "pazo",
-  "your age": 3,
-  "his tail": 1,
-  "to freinds": "two friends",
-};
-
-delete testObject["to freinds"];
-
-var munyanyu = testObject["to freinds"];
-console.log(munyanyu);
+function lookUp(val) {
+  var general = "";
+  var answer = {
+    first: "monday",
+    second: "tuesday",
+    third: "wednesday",
+    fourth: "thursday",
+    firth: "friday",
+  };
+  general = answer[val];
+  return general;
+}
+console.log(lookUp(second));
