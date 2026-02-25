b507e2101681e4852998e451dc588190310cfe1e
wase daniel <wasedaniel01@gmail.com>
2026-02-25 02:15:36 -0800

    accessing object using [] 16

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-function lookUp(val) {
-  var general = "";
-  var answer = {
-    1: "monday",
-    2: "tuesday",
-    3: "wednesday",
-    4: "thursday",
-    5: "friday",
-  };
-  general = answer[val];
-  return general;
+var objectList= {
+  1 : "one",
+  2 : "two",
+  3 : "three",
+  4 : "four",
 }
-console.log(lookUp(1));
+
+function checkObj(checkProp) [
+  if (objectList.hasOwnProperty(checkProp)  );
+  return checkObj
+]
