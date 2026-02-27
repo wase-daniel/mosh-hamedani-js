255be5d6dd6febf68b96448338213612d7fd15c7
wase daniel <wasedaniel01@gmail.com>
2026-02-27 10:51:46 -0800

    accessing nested object ex:18

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 for (let i = 0; i < 5; i++) {
   console.log("For loop iteration:", i);
 }
-// Output:
-// For loop iteration: 0
-// For loop iteration: 1
-// For loop iteration: 2
-// For loop iteration: 3
-// For loop iteration: 4
