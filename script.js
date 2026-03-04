7b39e4946ad0c68a9e5d902e5e59b481957b0a4a
wase daniel <wasedaniel01@gmail.com>
2026-03-04 01:45:47 -0800

    back on track

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var days = {
-  "my one": "monday",
-  "the two": "tuesday",
-  "for three": "wednesday",
-  "is four": "thursday",
-  "these five": "friday",
+var myStorage = {
+  car: {
+    inside: {
+      "glove box": "maps",
+      "passenger seat": "crubs",
+    },
+    outside: {
+      trunk: "jack",
+    },
+  },
 };
-
-var week = days["for three"];
-console.log(week);
