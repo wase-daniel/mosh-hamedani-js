a20550eb33dd97dbaa0428e9db841cbeafec7522
wase daniel <wasedaniel01@gmail.com>
2026-03-05 08:32:46 -0800

    forEach

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let i = 0;
-while (i < 10) {
-  i++;
-  if (i === 5) continue;
-  console.log(i);
-}
+var group = ["dog", "cat", "horse", "tiger"];
+
+group.forEach((animals) => {
+  console.log(animals);
+});
