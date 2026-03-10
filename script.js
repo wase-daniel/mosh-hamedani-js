34193241287506531ba31fb878a5db927af8a729
wase daniel <wasedaniel01@gmail.com>
2026-03-10 01:56:17 -0700

    using multi ternary operation ex 2 solved

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function ternaly(num) {
-  return num > 0 ? "positive" : num < 0 ? "negative" : num = 0 ? "zero";
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
 }
 console.log(ternaly(0));
