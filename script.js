e47096b2cd36de5011832c88b7337469e03b9bbf
wase daniel <wasedaniel01@gmail.com>
2026-03-11 02:42:24 -0700

    exploring lol 1

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function randomNumber(minNbr, maxNbr) {
   return Math.floor(Math.random() * (maxNbr - minNbr + 1)) + minNbr;
 }
 
-console.log(randomNumber(54654765, 5));
+console.log(randomNumber(10, 5));
 
-/*const username = "";
+const username = "";
 const message = document.getElementById("welcome-msg");
 
-message.textContent += username === "" ? " Guest" : username;*/
+message.textContent += username === "" ? " Guest" : username;
