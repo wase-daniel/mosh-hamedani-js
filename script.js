fbd2614b68d9e29fc586b77ddce22ee165bbb9f8
wase daniel <wasedaniel01@gmail.com>
2026-03-11 01:21:32 -0700

    learning DOM

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-function randomNumber(minNumber, maxNumber) {
-  return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-}
-
-console.log(randomNumber(1, 100));
+console.log(document);
