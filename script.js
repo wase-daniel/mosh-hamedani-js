33ce382e7f7fff3ba2cae1701b7c045492287743
wase daniel <wasedaniel01@gmail.com>
2026-03-12 07:06:56 -0700

    learning allow functions 3

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let theSumOf = (function () {
-  return function theSumOf(number, value = 1) {
+let numberCap = (function () {
+  return function numberCap(number, value = 1) {
     return number + value;
   };
 })();
-
-console.log(theSumOf(23, 5));
-console.log(theSumOf(5, 1));
+console.log(numberCap(12, 5));
+console.log(numberCap(120));
