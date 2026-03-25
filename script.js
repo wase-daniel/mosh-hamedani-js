df21588d24de3bb5ff1543a8e5fadccc68177f8a
wase daniel <wasedaniel01@gmail.com>
2026-03-25 03:59:31 -0700

    interactive now 1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let count  = 0;
-function increment(){
-    count = count + 1
-    document.getElementById("count-el").innerHTML  = count
+let count = 0;
+function increment() {
+  count = count + 1;
+  document.getElementById("count-el").innerHTML = count;
 }
