ab69a0d473f5e3c05d6806a7938bcf992863b929
wase daniel <wasedaniel01@gmail.com>
2026-03-27 00:28:04 -0700

    passanger counter str

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let welcomeEl = document.getElementById("welcome-el");
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
 
-let name = "itachi";
-let greeting = "welocome back ";
-welcomeEl.innerText = greeting + name;
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+}
 
-welcomeEl = welcomeEl + " 👋";
-
-welcome;
+function save() {
+  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+  let combinerBoth = count + " - ";
+  // 3. Render the variable in the saveEl using innerText
+  saveEl.innerText += combinerBoth;
+}
+// NB: Make sure to not delete the existing content of the paragraph
+console.log(count);
